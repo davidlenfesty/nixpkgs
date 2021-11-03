901ac31382aa3bbbb1b628c1f840c908728a6fa4
Milan Hauth <milahu@gmail.com>
2021-11-03 12:03:24 +0100

    qimgv: qt5to6 WIP runtime error: plugin wayland is not found

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28482,7 +28482,7 @@ with pkgs;
 
   qjackctl = libsForQt5.callPackage ../applications/audio/qjackctl { };
 
-  qimgv = libsForQt5.callPackage ../applications/graphics/qimgv { };
+  qimgv = libsForQt6.callPackage ../applications/graphics/qimgv { };
 
   qlandkartegt = libsForQt514.callPackage ../applications/misc/qlandkartegt {
     gdal = gdal.override {
